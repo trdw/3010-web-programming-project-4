33d7734780402fbcecf2e31e6d6c8bfdaf5fab8f
Duncan Werner <dawcf4@mail.umsl.edu>
2019-11-28 11:56:18 -0600

    Added database and table creation if not exists
    TODO:
    insertValidData code, confirmation page
    Testing

--- insertValidData.php
+++ insertValidData.php
@@ -5,39 +5,57 @@
                 "root", "");
 			// set the PDO error mode to exception
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sq1 = $conn->prepare ("CREATE DATABASE IF NOT EXISTS dogpeople");
-            $sq1 -> execute();
-			$sql = $conn->prepare("INSERT INTO registration 
-            (userName,
-            password,
-            passwordRepeat,
-            firstName,
-            lastName,
-            email,
-            phone,
-            birth,
-            address1,
-            address2,
-            city,
-            state,
-            zip,
-            gender,
-            maritalStatus)
-			VALUES (:userName, '', '', '', :website,
-			'', :comment, '', '', :email, :gender,
-			'', CURDATE())");
+            $conn->exec ("CREATE DATABASE IF NOT EXISTS dogpeople");
+            $conn->exec ("use dogpeople");
+
+                $result = $conn->exec ("CREATE TABLE IF NOT EXISTS registration (
+                ID INT AUTO_INCREMENT,
+                userName varchar(255) NOT NULL,
+                password varchar(255) NOT NULL,
+                passwordRepeat varchar(255) NOT NULL,
+                firstName varchar(255) NOT NULL,
+                lastName varchar(255) NOT NULL,
+                email varchar(255) NOT NULL,
+                phone varchar(255) NOT NULL,
+                birth date,
+                address1 varchar(255) NOT NULL,
+                address2 varchar(255) NOT NULL,
+                city varchar(255) NOT NULL,
+                state varchar(255) NOT NULL,
+                zip varchar(255) NOT NULL,
+                gender varchar(255) NOT NULL,
+                maritalStatus varchar(255) NOT NULL,
+                PRIMARY KEY (ID))");
+
+
+
+//			$sql = $conn->prepare("INSERT INTO registration
+//            (userName,
+//            password,
+//            passwordRepeat,
+//            firstName,
+//            lastName,
+//            email,
+//            phone,
+//            birth,
+//            address1,
+//            address2,
+//            city,
+//            state,
+//            zip,
+//            gender,
+//            maritalStatus)
+//			VALUES (:userName, '', '', '', :website,
+//			'', :comment, '', '', :email, :gender,
+//			'', CURDATE())");
 //			$sql->bindParam(':userName', $regForm["userName"][0]);
-//			$sql->bindParam(':website', $website);
-//			$sql->bindParam(':comment', $comment);
-//			$sql->bindParam(':email', $email);
-//			$sql->bindParam(':gender', $gender);
 //
 //			$sql->execute();
 			
 			$last_id = $conn->lastInsertId();
 			$_SESSION["last_id"] = "$last_id";
 			
-			header("Location: confirmation.php");
+			//header("Location: confirmation.php");
 		}
 		catch(PDOException $e) {
 			echo "Connection failed: " . $e->getMessage();
